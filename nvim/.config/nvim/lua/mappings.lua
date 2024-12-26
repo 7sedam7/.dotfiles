14c5351b0aa3a5a9f7acdbec67f4142a40bdb0d4
Joel Mislav Kunst <7sedam7@gmail.com>
2024-12-26 20:44:49 +0200

    Improved wording

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -21,7 +21,7 @@ map(
 	"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
 	{ desc = "Debugger set conditional breakpoint" }
 )
-map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+map("n", "<Leader>dx", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger terminate" })
 map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
 
 -- rustaceanvim
