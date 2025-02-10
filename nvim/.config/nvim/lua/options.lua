601321064a10fe1c8a180bb41e01c96d8180e658
Joel Mislav Kunst <7sedam7@gmail.com>
2025-02-10 23:49:32 +0200

    Added folding for markdown

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -14,3 +14,15 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.opt.relativenumber = true
+
+-- Folding
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    -- vim.opt_local.foldmethod = "syntax"
+    -- vim.opt_local.foldlevel = 1
+    vim.opt_local.foldmethod = "expr"
+    vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.opt_local.foldlevel = 99
+  end,
+})
